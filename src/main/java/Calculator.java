8c750119a0981e19d4691561dde30fe06768caf1
jypLord <jygold2n@naver.com>
2025-03-06 20:58:37 +0900

    제네릭 추가, 다중 연산 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,109 +3,66 @@ import java.util.NoSuchElementException;
 
 public class Calculator {
     private static final ArrayList<Object> calculateResults = new ArrayList<>();
-
-    private boolean whatDataType = true;
-
+    
     public void calculate(String[] inputOperators,String[] inputNumbers ){
 
-        long[] inputLong = new long[inputNumbers.length];
-        double[] inputDouble = new double[inputNumbers.length];
-
-        for(int j=0; j<inputNumbers.length; j++){
-            // 입력된 숫자에 소수점이 있는지 검증
+        Number[] numbers = new Number[inputNumbers.length];
+        
+        boolean whatDataType = true;
+        
+        for(int j=0; j<inputNumbers.length; j++) {
             whatDataType = inputNumbers[j].contains(".");
-
-            if(whatDataType){
-                inputDouble[j] = Double.parseDouble((inputNumbers[j]));
-            }else {
-                inputLong[j] = Long.parseLong(inputNumbers[j]);
+            if (whatDataType) {
+                numbers[j] = Double.parseDouble((inputNumbers[j]));
+            } else {
+                numbers[j] = Long.parseLong(inputNumbers[j]);
             }
         }
-
         for(int i=0; i<inputOperators.length; i++){
-            if(whatDataType){
+            if(whatDataType) {
                 switch (inputOperators[i]) {
                     case "+" -> {
-                        double result = add(inputDouble[i], inputDouble[i + 1]);
-                        inputDouble[i + 1] = result;
+                        double result = add(numbers[i], numbers[i + 1]);
+                        numbers[i + 1] = result;
                     }
 
                     case "-" -> {
-                        double result = subtract(inputDouble[i], inputDouble[i + 1]);
-                        inputDouble[i + 1] = result;
+                        double result = subtract(numbers[i], numbers[i + 1]);
+                        numbers[i + 1] = result;
                     }
                     case "*" -> {
-                        double result = multiply(inputDouble[i], inputDouble[i + 1]);
-                        inputDouble[i + 1] = result;
+                        double result = multiply(numbers[i], numbers[i + 1]);
+                        numbers[i + 1] = result;
                     }
                     case "/" -> {
-                        double result = divide(inputDouble[i], inputDouble[i + 1]);
-                        inputDouble[i + 1] = result;
+                        double result = divide(numbers[i], numbers[i + 1]);
+                        numbers[i + 1] = result;
                     }
                 }
-            }else {
-                switch (inputOperators[i]) {
-                    case "+" -> {
-                        long result = add(inputLong[i], inputLong[i + 1]);
-                        inputLong[i + 1] = result;
-                    }
-
-                    case "-" -> {
-                        long result = subtract(inputLong[i], inputLong[i + 1]);
-                        inputLong[i + 1] = result;
-                    }
-                    case "*" -> {
-                        long result = multiply(inputLong[i], inputLong[i + 1]);
-                        inputLong[i + 1] = result;
-                    }
-                    case "/" -> {
-                        long result = divide(inputLong[i], inputLong[i + 1]);
-                        inputLong[i + 1] = result;
-                    }
-                }
-            }
+            }    
         }
-        calculateResults.add(inputLong[inputLong.length-1]);
-        System.out.println(inputLong[inputLong.length-1]);
+        calculateResults.add(numbers[numbers.length-1]);
+        System.out.println(numbers[numbers.length-1]);
     }
-    public long add(long inputLong1, long inputLong2) {
-        return  inputLong1+ inputLong2;
-    }
-    public double add(double inputDouble1, double inputDouble2) {
-        return  inputDouble1+ inputDouble2;
+    public <T extends Number> double add(T inputNum1, T inputNum2) {
+        return  inputNum1.doubleValue() + inputNum2.doubleValue();
     }
 
-
-    public long subtract(long inputLong1, long inputLong2) {
-        return  inputLong1 - inputLong2;
-    }
-    public double subtract(double inputDouble1, double inputDouble2) {
-        return  inputDouble1 - inputDouble2;
+    public <T extends Number> double subtract(T inputNum1, T inputNum2) {
+        return  inputNum1.doubleValue() - inputNum2.doubleValue();
     }
 
-    public long multiply(long inputLong1, long inputLong2) {
-        return  inputLong1 * inputLong2;
-    }
-    public double multiply(double inputDouble1, double inputDouble2) {
-        return inputDouble1 * inputDouble2;
+    public <T extends Number> double multiply(T inputNum1, T inputNum2) {
+        return  inputNum1.doubleValue() * inputNum2.doubleValue();
     }
 
-
-    public long divide(long inputLong1, long inputLong2) {
-        if (inputLong2 == 0) {
+    public <T extends Number> double divide(T inputNum1, T inputNum2) {
+        if (inputNum2.doubleValue() == 0) {
             throw new ArithmeticException("Can't divide to 0");
         } else {
-            return  inputLong1/inputLong2;
+            return  inputNum1.doubleValue()/inputNum2.doubleValue();
         }
     }
-    public double divide(double inputDouble1, double inputDouble2) {
-        if (inputDouble2 == 0) {
-            throw new ArithmeticException("Can't divide to 0");
-        } else {
-            return  inputDouble1/inputDouble2;
-        }
-    }
-
 
 
     public static ArrayList<?> getResult() {
